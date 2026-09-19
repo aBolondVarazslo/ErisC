fcdeab6cd99d9eff97ceddb559b3a59e95f07be5
aBolondVarazslo <abolondvarazslo@gmail.com>
2026-09-19 19:24:14 +0100

    Added keywords to TokenKind enum

--- src/lexer.h
+++ src/lexer.h
@@ -5,6 +5,18 @@ typedef enum {
     TOK_EOF, /* No more input/End Of File */
     TOK_INT, /* Integer literal */
     TOK_IDENT, /* Identifier */
+
+    /* Keywords */
+    TOK_FN, /* Function keyword */
+    TOK_LET, /* Let keyword */
+    TOK_RET, /* Return keyword */
+    TOK_IF, /* If keyword */
+    TOK_ELSE, /* Else keyword */
+    TOK_WHILE, /* While keyword */
+    TOK_FOR, /* For keyword */
+    TOK_BRIDGE, /* Extern keyword */
+    TOK_TRUE, /* True keyword */
+    TOK_FALSE, /* False keyword */
 } TokenKind;
 
 typedef struct {
